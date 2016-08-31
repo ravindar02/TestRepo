aad04973ef5acc3d1ad640254dfa9272afe562bc
Ravindar D <RavindarD@chenoainc.com>
2016-08-31 17:43:22 +0530

    Test Merge

--- BasicJava/src/com/rd/HelloWorld.java
+++ BasicJava/src/com/rd/HelloWorld.java
@@ -4,8 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
-		System.out.println("Hello1");
-		System.out.println("addition line for newissue");
+		System.out.println("Hello New Issue");
 	}
 
 }
