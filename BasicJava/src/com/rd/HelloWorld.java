2bdd2b4b93efcaf7541f79ea0d3995631293d141
Ravindar D <RavindarD@chenoainc.com>
2016-08-31 16:21:37 +0530

    new branch

--- BasicJava/src/com/rd/HelloWorld.java
+++ BasicJava/src/com/rd/HelloWorld.java
@@ -3,8 +3,9 @@ package com.rd;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
 		System.out.println("Hello1");
+		int i=100;
+		System.out.println(i);
 	}
 
 }
