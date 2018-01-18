f3395bfce1e205c586b14e56db003a30ee52469f
dayachan <ravindar.dayachand@here.com>
2018-01-18 18:06:42 +0530

    ammending the change

--- BasicJava/src/com/rd/HelloWorld.java
+++ BasicJava/src/com/rd/HelloWorld.java
@@ -6,6 +6,8 @@ public class HelloWorld {
 		System.out.println("Hello1");
 		System.out.println("addition line for newissue");
 		System.out.println("New addition on Jan 18");
+        System.out.println("New addition on Jan 18 Repo1");
+		System.out.println("New addition on Jan 18 Repo2");
 		int i=100;
 		System.out.println(i);
 	}
