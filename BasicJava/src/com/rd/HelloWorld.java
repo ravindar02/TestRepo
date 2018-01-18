73093c2e4e679c95b6ffb48dcec4e3769b7322b4
dayachan <ravindar.dayachand@here.com>
2018-01-18 18:01:02 +0530

    2018testing

--- BasicJava/src/com/rd/HelloWorld.java
+++ BasicJava/src/com/rd/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello1");
 		System.out.println("addition line for newissue");
+		System.out.println("New addition on Jan 18");
 		int i=100;
 		System.out.println(i);
 	}
