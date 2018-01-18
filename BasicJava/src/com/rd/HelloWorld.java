bc388a3fd724761da5480bb771d2a24fb149e74b
dayachan <ravindar.dayachand@here.com>
2018-01-18 18:07:57 +0530

    Conflicting file

--- BasicJava/src/com/rd/HelloWorld.java
+++ BasicJava/src/com/rd/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
 		System.out.println("addition line for newissue");
 		System.out.println("New addition on Jan 18");
         System.out.println("New addition on Jan 18 Repo1");
+		System.out.println("New addition on Jan 18 Repo2");
 		int i=100;
 		System.out.println(i);
 	}
